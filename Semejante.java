83cf8acf24aa5edd163d3dfe70951970b64eb9e5
pag753 <pag753@gmail.com>
2017-12-11 20:12:57 -0600

    Update Semejante.java

--- Semejante.java
+++ Semejante.java
@@ -5,5 +5,6 @@ public class Semejante
 	public static void main(String[] args)
 	{
 		JOptionPane.showMessageDialog("HOLA MUNDO CRUEL");
+		JOptionPane.showMessageDialog("HOLA MUNDO CRUEL2");
 	}
 }
